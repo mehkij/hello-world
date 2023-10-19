7b05b4acc08de13caba629ed9133b11b730d2baf
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-10-19 06:18:15 -0400

    Fix main.exe
    Terminal would flash open and then immediately close

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <limits>
 
 int main()
 
 {
   std::cout << "Hello, world!";
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find a newline
+  std::cin.get(); // get one more char from the user (waits for suer to press enter)
   return 0;
 }
